fd3e3d0c3af2cf2b0787fd4adafdfb7585336c9c
Michael-0208 <vikash.webroot.0208@gmail.com>
2025-04-04 01:05:22 +0530

    initial commit

---
+++ routes/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('login', function (AuthKitLoginRequest $request) {
+    return $request->redirect();
+})->middleware(['guest'])->name('login');
+
+Route::get('authenticate', function (AuthKitAuthenticationRequest $request) {
+    return tap(to_route('dashboard'), fn () => $request->authenticate());
+})->middleware(['guest']);
+
+Route::post('logout', function (AuthKitLogoutRequest $request) {
+    return $request->logout();
+})->middleware(['auth'])->name('logout');
